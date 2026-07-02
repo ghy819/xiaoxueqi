f0eaad321a1a62387ca0477b9b6d874efcfe68c7
ghy819 <3299134637@qq.com>
2026-07-02 09:11:18 +0800

    test0

--- push.bat
+++ push.bat
@@ -1,12 +1,41 @@
 @echo off
-echo ===== Git 自动上传 =====
+setlocal
 
-git add .
+echo ===== Git Auto Upload =====
 
-set /p msg=请输入提交说明:
-git commit -m "%msg%"
+call git add .
+if errorlevel 1 goto error
 
-git push
+set "msg="
+set /p "msg=Commit message: "
+if not defined msg (
+    echo Commit message cannot be empty.
+    goto end
+)
 
-echo ===== 完成 =====
+call git commit -m "%msg%"
+if errorlevel 1 goto error
+
+call git push
+if errorlevel 1 goto error
+
+set "tag="
+set /p "tag=Tag (example: v1.1, press Enter to skip): "
+if not defined tag goto success
+
+call git tag -a "%tag%" -m "%msg%"
+if errorlevel 1 goto error
+
+call git push origin "%tag%"
+if errorlevel 1 goto error
+
+:success
+echo ===== Done =====
+goto end
+
+:error
+echo ===== Failed - check the messages above =====
+
+:end
 pause
+endlocal
