b2291de479fdb8a288c5d9cce2c23e4252a788f0
ghy819 <3299134637@qq.com>
2026-07-03 08:49:05 +0800

    直线弯道判断，减小直线摆动角度

--- track.h
+++ track.h
@@ -61,9 +61,9 @@
 /* 顺时针椭圆赛道的直道/弯道识别与直道稳定参数 */
 #define TRACK_CURVE_ENTER_COUNT 2   /* OUT4 连续 2 次后进入弯道模式    */
 #define TRACK_STRAIGHT_ENTER_COUNT 20 /* 回中约 60ms 后进入直道模式     */
-#define TRACK_STRAIGHT_SMALL_STEER_RATIO 40 /* 直道轻微偏差转向量       */
-#define TRACK_STRAIGHT_INNER_STEER_RATIO 35 /* 直道 OUT2/OUT4 转向量    */
-#define TRACK_STRAIGHT_SERVO_OLD_WEIGHT 4
+#define TRACK_STRAIGHT_SMALL_STEER_RATIO 30 /* 直道轻微偏差转向量       */
+#define TRACK_STRAIGHT_INNER_STEER_RATIO 30 /* 直道 OUT2/OUT4 转向量    */
+#define TRACK_STRAIGHT_SERVO_OLD_WEIGHT 5
 #define TRACK_STRAIGHT_SERVO_NEW_WEIGHT 1
 
 /* 舵机转向范围: 以 90° 为中位，限制机械摆角避免急甩和轮胎阻力 */
